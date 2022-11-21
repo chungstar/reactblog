b469009825913833f1fe482d3bfdd0d1823b6eb5
정현규 <gusrb0511@gmail.com>
2022-11-21 23:54:46 +0900

    modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  let post = "강남 우동 맛집"
-  
+
+  let [title1, titleChange] = useState(['남자 코트 추천','여자 코트 추천','남자 바지 추천']);
+  let [likeButton, likeChange] = useState([0,0,0]);
+  let [modal, setModal] = useState(false);
+
   return (
     <div className="App">
-      <div className='black-nav'>
-        <h4 style={{color:'red',fontSize:'15px'}}>다시 해보는 블로그</h4>
+
+      <div className="black-nav">
+        <h4>blogreact</h4>
       </div>
-      <h4>{ post }</h4>
+      {/* <div className="list">
+        <h4>{ title1[0] } <span onClick={ () => { likeChange(likeButton+1) } }>👍</span>{ likeButton }
+        <button onClick={ ()=>{
+          let copy = [...title1];
+          copy.sort();
+          titleChange(title1)
+        } }> 정렬버튼 </button>
+        </h4>
+        <p>시간</p>
+      </div>
+      <div className="list">
+        <h4>{ title1[1] }</h4>
+        <p>시간</p>
+      </div>
+      <div className="list">
+        <h4>{ title1[2] }</h4>
+        <p>시간</p>
+      </div> */}
+
+      {
+        title1.map(function(a, i){
+          return(
+            <div className="list" key={i}>
+             <h4 onClick={()=>{setModal(!modal)}}>{ title1[i] }</h4>
+             <span onClick={ () => {
+                let copy = [...likeButton];
+                copy[i] = likeButton[i]+1;
+                likeChange(copy)
+                } }>👍</span>{ likeButton[i] }
+             
+             <p>시간</p>
+            </div>
+          )
+        })
+      }
+
+      {
+        modal == true ? <Modal></Modal> : null
+      }
+      
+
     </div>
   );
+  
+}
+
+function Modal(){
+  return(
+      <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+
+  )
 }
 
 export default App;
